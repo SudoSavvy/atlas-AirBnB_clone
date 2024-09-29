d94816c622b586ccc7d1c3c1e359a9300e33e4a9
SudoSavvy <donovanjheape@gmail.com>
2024-09-29 16:23:48 -0500

    Bug fixing code for the checker

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,37 +2,30 @@ import json
 from models.base_model import BaseModel
 
 class FileStorage:
-    """Serializes instances to a JSON file & deserializes JSON file to instances."""
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
+        """Return the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        """Set in __objects the obj with key <obj class name>.id."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file."""
+        """Serialize __objects to the JSON file."""
         obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w') as f:
+        with open(self.__file_path, "w") as f:
             json.dump(obj_dict, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects (if it exists)."""
+        """Deserialize the JSON file to __objects (if it exists)."""
         try:
-            with open(self.__file_path, 'r') as f:
+            with open(self.__file_path, "r") as f:
                 obj_dict = json.load(f)
                 for key, value in obj_dict.items():
-                    class_name = value["__class__"]
-                    # Dynamically get the class from globals or import
-                    cls = globals().get(class_name)
-                    if cls:
-                        self.__objects[key] = cls(**value)
-        except FileNotFoundError:
-            pass
+    
+
 
