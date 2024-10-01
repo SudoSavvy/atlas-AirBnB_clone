6d1d6e6f2f738e01157cd7ad6ca7b80f7228a4b7
SudoSavvy <donovanjheape@gmail.com>
2024-10-01 10:44:10 -0500

    Fixing for checker grades

--- console.py
+++ console.py
@@ -82,21 +82,21 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")  # Error message for missing instance
 
     def do_update(self, line):
-        """Updates an instance based on class name and id by adding or updating attribute."""  # Documentation for the update command
+        """Updates an instance based on class name and id by adding or updating attribute."""
         if not line:  # Check if the line is empty
             print("** class name missing **")  # Error message for missing class name
             return  # Exit the function
 
         args = line.split()  # Splitting the input line into arguments
-        if len(args) < 3:  # Check if instance ID is provided
+        if len(args) < 2:  # Check if instance ID is provided
             print("** instance id missing **")  # Error message for missing instance ID
             return  # Exit the function
 
-        if len(args) < 4:  # Check if attribute name is provided
+        if len(args) < 3:  # Check if attribute name is provided
             print("** attribute name missing **")  # Error message for missing attribute name
             return  # Exit the function
 
-        if len(args) < 5:  # Check if value is provided
+        if len(args) < 4:  # Check if value is provided
             print("** value missing **")  # Error message for missing value
             return  # Exit the function
 
@@ -119,6 +119,7 @@ class HBNBCommand(cmd.Cmd):
 
         setattr(obj, attr_name, attr_value)  # Setting the attribute value on the object
         obj.save()  # Saving the changes
+
 
     def do_quit(self, line):
         """Exits the command interpreter."""  # Documentation for the quit command
